97b776e9993474438604376d29940e78ceb4a7c8
GINOFM <facundogino@outlook.com>
2019-01-10 23:37:47 -0300

    34

---
+++ html5game/tph_fullScreen.js
@@ -0,0 +1,15 @@
+/* Get the element you want displayed in fullscreen */ 
+var elem = document.getElementById("canvas");
+
+/* Function to open fullscreen mode */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+  elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+  elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+  elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+  elem.msRequestFullscreen();
+  }
+}
